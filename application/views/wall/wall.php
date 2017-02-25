464577ec1ec16767a6a459aa178c99dc8f01a910
Aatu Mikkonen <t5miaa00@students.oamk.fi>
2017-02-25 18:53:39 +0200

    Made the post-it note basis working. Changed the styles of modal, won't be needing them like that anyways... probably

--- application/views/wall/wall.php
+++ application/views/wall/wall.php
@@ -1,7 +1,33 @@
 <div class="container-flow">
    <div class="corkwall">
-      <div class="post-note" style="top: 120px; left: 233px;">
+      <div class="post-note" style="top: 120px; left: 233px;"
+           data-toggle="modal" data-target="#postID-1">
          <p>Ferst post pitses!</p>
+         <div class="post-note-footer">
+            <p>Aatu</p>
+         </div>
       </div>
+
+      <!-- MODAL START -->
+      <div class="modal fade" id="postID-1" tabindex="-1" role="dialog">
+         <div class="modal-dialog" role="document">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <a href="#" class="delete-post"><span class="glyphicon glyphicon-trash"></span>Delete post?</a>
+                  <button type="button" class="close" data-dismiss="modal">
+                     &times;
+                  </button>
+               </div>
+               <div class="modal-body">
+                  Ferst post pitses!
+               </div>
+               <div class="modal-footer">
+                  ~Aatu
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- MODAL END -->
+
    </div>
 </div>
